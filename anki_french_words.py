31fd1a0e1f37479b910136c3317c26405d3b799d
Gabriel Franzeri <gabrielfranzer@gmail.com>
2021-05-22 11:50:56 -0300

    rodar navegador em segundo plano

--- anki_french_words.py
+++ anki_french_words.py
@@ -1,3 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+# para abrir o navegador 
+# nav = webdriver.Chrome()
+
+# rodar navegador em segundo plano
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.headless = True
+nav = webdriver.Chrome(options=chrome_options)
+
