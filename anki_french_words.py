355f5c4902812cabf906a23d4df29e8e191f189b
Gabriel Franzeri <gabrielfranzer@gmail.com>
2021-05-22 12:57:36 -0300

    importants selenium functions in comments

--- anki_french_words.py
+++ anki_french_words.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 # para abrir o navegador 
 # nav = webdriver.Chrome()
@@ -11,7 +12,10 @@ chrome_options.headless = True
 nav = webdriver.Chrome(options=chrome_options)
 
 # funções do webdriver
-nav.get(url)
-nav.find_element_by_xpath(codigoxpath).send_keys('escrita pra pesquisa')
-nav.find_element_by_xpath(codigoxpath).send_keys(Keys.ENTER)
-variavel = nav.find_element_by_xpath(codigoxpath).get_attribute('data-value')
+# nav.get(url)
+# nav.find_element_by_xpath(codigoxpath).send_keys('escrita pra pesquisa')
+# nav.find_element_by_xpath(codigoxpath).send_keys(Keys.ENTER)
+# variavel = nav.find_element_by_xpath(codigoxpath).get_attribute('data-value')
+# variavel = nav.find_element_by_xpath(codigoxpath).text
+# nav.quit()
+# time.sleep(1)
