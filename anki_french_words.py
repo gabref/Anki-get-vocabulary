b96f819a5be05805f904ae0f8eef8f49c4e9b2db
Gabriel Franzeri <gabrielfranzer@gmail.com>
2021-05-22 20:15:05 -0300

    added function to search google images

--- anki_french_words.py
+++ anki_french_words.py
@@ -3,13 +3,23 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 # para abrir o navegador 
-# nav = webdriver.Chrome()
+nav = webdriver.Chrome()
 
 # rodar navegador em segundo plano
-from selenium.webdriver.chrome.options import Options
-chrome_options = Options()
-chrome_options.headless = True
-nav = webdriver.Chrome(options=chrome_options)
+# from selenium.webdriver.chrome.options import Options
+# chrome_options = Options()
+# chrome_options.headless = True
+# nav = webdriver.Chrome(options=chrome_options)
+
+word = 'chien'
+
+# BAIXAR IMAGEM DO GOOGLE IMAGENS
+from testImage import search_google
+search_google(word)
+
+
+
+# nav.quit()
 
 # funções do webdriver
 # nav.get(url)
